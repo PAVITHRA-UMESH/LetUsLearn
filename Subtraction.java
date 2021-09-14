84874fecfe617830fcbc2c28c90bb202b0826d60
PAVITHRA U <pavithrau2000@gmail.com>
2021-09-14 14:04:10 +0530

    Subtraction.java Modified

--- Subtraction.java
+++ Subtraction.java
@@ -2,6 +2,6 @@ public class Addition{
 public static void main(String[] args){
                 int a,b,c;
                 c=a-b;
-                System.out.println("Difference:"+c);
+                System.out.println("Difference of a and b:"+c);
               }
 }
